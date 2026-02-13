c292bcc56106d3e33d44c4251848a717bb1d1186
samprit <samprit888@icloud.com>
2026-02-13 10:19:48 +0530

    Update base path for XCLIPPER repository

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     return {
-      base: '/XCLIPPER-main/',
+      base: '/XCLIPPER/',
       server: {
         port: 5500,
         host: '0.0.0.0',
